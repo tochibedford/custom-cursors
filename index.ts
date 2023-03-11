cedb3dede3438f8912386d932ee4920f7c51584f
tochibedford <musicofbyte@gmail.com>
2023-03-11 21:54:31 +0100

    feat: syncAnimate routine to animate pointers

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const objects: Pointer[] = [];
+
 const mouse = {
     x: window.innerWidth / 2,
     y: window.innerHeight / 2
@@ -9,6 +11,7 @@ class Pointer {
     private _dx: number // difference between mouse poition and current x position
     private _dy: number
     private _speed: number // 1 is normal
+    public _animate: boolean
     public _element: HTMLElement
 
     constructor(x: number, y: number, dx: number, dy: number, speed: number, element: HTMLElement) {
@@ -18,6 +21,7 @@ class Pointer {
         this._dy = 0
         this._speed = speed
         this._element = element
+        this._animate = true
         element.style.cssText =
             `
                 position: absolute; 
@@ -32,7 +36,12 @@ class Pointer {
         this._element.style.top = `${this._y - this._element.getBoundingClientRect().height / 2}px`
     }
 
-    update() {
+    update(_mouse: { x: number, y: number }) {
+        //difference in distance
+        this._dx = _mouse.x - this._x
+        this._dy = _mouse.y - this._y
+
+        //modify position based on difference
         this._x += this._dx * this._speed
         this._y += this._dy * this._speed
         this.draw()
@@ -40,9 +49,18 @@ class Pointer {
 
 }
 
-function animate() {
+//animates all pointers
+function syncAnimate(time: DOMHighResTimeStamp) {
+    objects.forEach(pointer => {
+        if (pointer._animate) {
+            pointer.update(mouse)
+        }
+    })
+
+    requestAnimationFrame(syncAnimate)
 
 }
+
 
 // window.addEventListener('mousemove', (event) => {
 //     mouse.x = event.clientX
